d88f9a2d006ae36ffff7f955247d855b6ef02930
Dhrumil Amish Shah <dh416386@dal.ca>
2021-12-05 02:37:37 -0400

    Completed User management and Authentication module - 3 Factor Authentication

---
+++ src/components/Home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import useStyles from './styles';
+
+export const Home = () => {
+    const userReceivedProps = useLocation().state;
+    console.log(userReceivedProps);
+    return (
+        <div>
+            <div>Home Page</div>
+            <div>{userReceivedProps.firstName}</div>
+            <div>{userReceivedProps.lastName}</div>
+            <div>{userReceivedProps.email}</div>
+            <div>{userReceivedProps.answer1}</div>
+            <div>{userReceivedProps.answer2}</div>
+            <div>{userReceivedProps.answer3}</div>
+        </div>
+    );
+};
